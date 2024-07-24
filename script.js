fc0d809169158aea92f9c457050dbd12accd8cb3
MIKNSJ <sculetoncode@gmail.com>
2024-07-23 21:22:45 -0700

    fixed animation for when closing dropdown nav menu

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 function clickMenuButton() {
     document.getElementById("nav_icon").classList.toggle("change");
     document.getElementById("dd_menu").classList.toggle("show");
-    document.getElementById("menu_01").classList.toggle("show_menu_options");
-    document.getElementById("menu_02").classList.toggle("show_menu_options");
-    document.getElementById("menu_03").classList.toggle("show_menu_options");
 }
 
 window.onclick = function(event) {
@@ -15,15 +12,6 @@ window.onclick = function(event) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains("show")) {
                 openDropdown.classList.remove("show");
-            }
-        }
-
-        var dropdowns = document.getElementsByClassName("menu_options");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show_menu_options")) {
-                openDropdown.classList.remove("show_menu_options");
             }
         }
 
